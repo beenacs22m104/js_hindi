cbcde16a43afabeb18d9629ea2bbe2e2844282cf
beenacs22m104 <123085929+beenacs22m104@users.noreply.github.com>
2023-12-17 09:54:35 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,9 @@
+let name="beena"
+let age=18
+let result= true
+//number
+//null->object
+//string
+//boolean
+//undefined
+console.log(typeof "beens");
---
+++ 01_basics/variables.js
@@ -0,0 +1,7 @@
+const account_Id=14455
+let account_name="beena56"
+var account_password="1234"
+account_city="jaipur";
+let account_state;
+//use let always same as var but var has block and function scope problem.
+console.table([account_Id,account_name,account_password,account_city,account_state])
