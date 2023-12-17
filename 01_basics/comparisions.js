1abb3f98c8678f3b0d9fbd98231a65b47e007204
beenacs22m104 <123085929+beenacs22m104@users.noreply.github.com>
2023-12-17 11:16:41 +0000

    compare

---
+++ 01_basics/comparisions.js
@@ -0,0 +1,10 @@
+// console.log(2>1);
+// console.log(2<=1);
+// console.log(2==1);
+
+// console.log("2">1);//true converted string to number 
+console.log(null>0);//false
+console.log(null==0);//false
+console.log(null>=0);//true 
+
+console.log("2"===1);//false strictly comparing
