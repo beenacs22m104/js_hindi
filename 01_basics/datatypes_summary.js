922659ed1c0de46f594e19cdab3779fa0c0c2e65
beenacs22m104 <123085929+beenacs22m104@users.noreply.github.com>
2023-12-18 04:53:19 +0000

    summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,19 @@
+//primitive datatypes
+//string, Boolean , Number , Null , undefined , symbol , Bigint
+  
+
+// const score=Symbol(188)
+// const scoreid=Symbol(188)
+// console.log(score===scoreid);
+
+//reference types-non primitive 
+//array , objects , functions
+const heroes = ["shaktiman","dogo","powwer"]
+const obj={
+    name: "beena",
+     age : 18
+}
+const myfunction=function()
+{
+    console.log("hello world");
+}
