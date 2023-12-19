1d7e426e363ee71fc4097bf1ab6e7854efee3fb7
beenacs22m104 <123085929+beenacs22m104@users.noreply.github.com>
2023-12-19 05:29:33 +0000

    new

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -8,12 +8,26 @@
 
 //reference types-non primitive 
 //array , objects , functions
-const heroes = ["shaktiman","dogo","powwer"]
-const obj={
-    name: "beena",
-     age : 18
+// const heroes = ["shaktiman","dogo","powwer"]
+// const obj={
+//     name: "beena",
+//      age : 18
+// }
+// const myfunction=function()
+// {
+//     console.log("hello world");
+// }
+//*******************************stack-primitive , heap-reference ************************* */
+// let username="beena@iitdotcom";
+// let username2=username;
+// username2="sanjay@ok"
+// console.log(username);
+// console.log(username2);
+let user1={
+    email:"beena@ok",
+    age:18
 }
-const myfunction=function()
-{
-    console.log("hello world");
-}
+let user2=user1;
+user2.email="sanjay@ok";
+console.log(user1.email);
+console.log(user2.email);
